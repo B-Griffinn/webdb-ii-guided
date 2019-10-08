63ca75bddf80299cef35457182c26335cb8a622a
Ben <bigriffin94@outlook.com>
2019-10-08 09:58:18 -0700

    npm i -g knex, knex init

---
+++ knexfile.js
@@ -0,0 +1,47 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    // client - database briver
+    client: 'sqlite3',
+    connection: {
+      filename: './data/produce.db3',
+    },
+    // SQLite3 requires this
+    useNullAsDefault: true,
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
